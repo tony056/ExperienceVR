a1182ada69e51814030d63258294945be000cb9c
aldrich1221 <aldrich1221@gmail.com>
2018-03-11 21:06:51 +0800

    delete mega fantasy

---
+++ Assets/cube_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A))
+			transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 100.0f, Space.World);
+	}
+}
